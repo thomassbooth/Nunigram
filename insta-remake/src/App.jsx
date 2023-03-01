615d9f1952619931c6fa0aaa73e38970dadaa784
Thomas Booth <thomasbooth26@gmail.com>
2023-03-01 00:41:42 +0000

    Update these

--- insta-remake/src/App.jsx
+++ insta-remake/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Nav from './Components/Nav/Nav'
-import Center from './Components/Center/Center'
+import Nav from './Containers/Nav'
+import Center from './Containers/Center'
+
 
 function App() {
-
   return (
     <div>
       <Nav/>
--- insta-remake/src/main.jsx
+++ insta-remake/src/main.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-wzza37ptt1vexeyh.eu.auth0.com"
+    clientId="kyiy0YY80cnRmMh7l7HewpTDWItNvEI0"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  </Auth0Provider>
 )
+
+// ReactDOM.render(
+//   <Auth0Provider
+//     domain="dev-wzza37ptt1vexeyh.eu.auth0.com"
+//     clientId="kyiy0YY80cnRmMh7l7HewpTDWItNvEI0"
+//     authorizationParams={{
+//       redirect_uri: window.location.origin
+//     }}
+//   >
+//     <React.StrictMode>
+//       <App/>
+//     </React.StrictMode>
+//   </Auth0Provider>,
+//   document.getElementById("root")
+// );
