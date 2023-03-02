64112117c67bc413fe61aefda10243fe3dfb4fe2
Thomas Booth <thomasbooth26@gmail.com>
2023-03-02 19:56:57 +0000

    Add center right

---
+++ insta-remake/src/Containers/Center_Right.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Profile from '../Components/Auth0/Auth0profile'
+
+function Right() {
+  return (
+    <div className = 'flex'>
+      <Profile/>
+    </div>
+  )
+}
+
+export default Right
