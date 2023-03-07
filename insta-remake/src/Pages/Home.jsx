de9e7868158c91401ae233e853fddf0a58e18fa1
Thomas Booth <thomasbooth26@gmail.com>
2023-03-07 20:49:42 +0000

    Split into pages

---
+++ insta-remake/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Nav from '../Containers/Nav'
+import Center from '../Containers/Center'
+
+
+function Home() {
+  return (
+    <div>
+        <Center/>
+    </div>
+  )
+}
+
+export default Home
