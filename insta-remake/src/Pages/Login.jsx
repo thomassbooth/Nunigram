d69bbec51472ff9b5522af845bc10777cd0eef2f
Thomas Booth <thomasbooth26@gmail.com>
2023-03-08 20:09:01 +0000

    New Pages and Data protection route protection

---
+++ insta-remake/src/Pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginButton from '../Components/Auth0/Auth0login'
+
+function Login() {
+  return (
+    <div>
+        <LoginButton/>
+    </div>
+  )
+}
+
+export default Login
