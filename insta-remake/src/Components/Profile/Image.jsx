d6622e2e710ec4d3e7e5739a8b1760330bfb57ed
Thomas Booth <thomasbooth26@gmail.com>
2023-03-10 01:10:11 +0000

    image component to split logic

---
+++ insta-remake/src/Components/Profile/Image.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Image({data, user, watchedStory}) {
+    let storyClass = ''
+    if (!data.story.watched){
+        storyClass = 'ring-offset-[5px] ring-rose-900 ring-[3.5px]'
+    } else {
+        storyClass = 'ring-offset-[5px] ring-grey-500 ring-[2px]'
+    }
+  return (
+    <div>
+        {data.story.exists ? 
+            <button onClick = {() => {watchedStory()}} className = 'mr-20 ml-10'>
+                <img src = 'images/cat.jpg' className = {'w-[175px] rounded-full border border-gray-500 ' + storyClass}></img>
+            </button> : <div className = 'mr-20 ml-10'><img src = 'images/cat.jpg' className = 'w-[175px] rounded-full border border-gray-500'></img></div>}
+    </div>
+  )
+}
+
+export default Image
