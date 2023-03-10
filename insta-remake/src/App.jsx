aba02fc6f00899ab00cb71c20e86cf1a48933cac
Thomas Booth <thomasbooth26@gmail.com>
2023-03-10 18:28:53 +0000

    update to router

--- insta-remake/src/App.jsx
+++ insta-remake/src/App.jsx
@@ -8,44 +8,41 @@ import { useAuth0 } from '@auth0/auth0-react';
 import Explore from './Pages/Explore'
 import Login from './Pages/Login'
 import { AuthenticationGuard } from './Components/Auth0/Authentication-guard'
-
-
-function App() {
-  const {isAuthenticated, isLoading} = useAuth0();
-
   
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path = '/' element = {<Root isLoading = {isLoading} isAuthenticated = {isAuthenticated}/>}>
-        <Route index element = {<Home/>} />
-        <Route path = '/message' element = {<AuthenticationGuard component = {Messages}/>} />
-        <Route path = '/explore' element = {<Explore/>} />
-        <Route path = ':handle' element = {<Profile/>} />
-      </Route>
+  function App() {
+    const {isAuthenticated, isLoading} = useAuth0();
+  
+    console.log(isAuthenticated)
+    const router = createBrowserRouter(
+      createRoutesFromElements(
+        <Route path = '/' element = {<Root/>}>
+          <Route index element = {<Home/>} />
+          <Route path = '/message' element = {<AuthenticationGuard component = {Messages}/>} />
+          <Route path = '/explore' element = {<Explore/>} />
+          <Route path = ':handle' element = {<Profile/>} />
+        </Route>
+      )
     )
-  )
-  return (
-    <div className = 'flex'>
-      <RouterProvider router = {router} />
-    </div>
-  )
-}
-
-export default App
-
-const Root = ({isLoading, isAuthenticated}) => {
-  return(
-    
-    <>
-      {isAuthenticated || isLoading? 
-      <div>
-        <Nav/>
-        <div className = 'w-screen flex justify-center '>
-          <div className = 'w-1/5'>
-          </div>
-          <Outlet/>
-        </div> 
-      </div> : <Login/>}
-    </>
-  )
-}
+    return (
+      <div className = 'flex'>
+        {isAuthenticated || isLoading ? <RouterProvider router = {router} />: <Login/>}
+      </div>
+    )
+  }
+  
+  export default App
+  
+  const Root = () => {
+    return(
+      <>
+        <div>
+          <Nav/>
+          <div className = 'w-screen flex justify-center'>
+            <div className = 'w-1/5'>
+            </div>
+            <Outlet/>
+          </div> 
+        </div>
+      </>
+    )
+  }
