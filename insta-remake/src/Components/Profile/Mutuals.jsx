6a8fdec0a971a7b6fc66f5a4e64d8ec6b426e52b
Thomas Booth <thomasbooth26@gmail.com>
2023-03-10 21:55:12 +0000

    Mutuals component

---
+++ insta-remake/src/Components/Profile/Mutuals.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Mutuals({mutualFollowers}) {
+    console.log(mutualFollowers)
+    let followedByMiddle = mutualFollowers.slice(0, 3).join(', ')
+    
+    let followedByEnd = ''
+    if (mutualFollowers.length > 3){
+      followedByEnd = ' + ' + String(mutualFollowers.length - 3) + ' more'
+    }
+  return (
+    <div className = 'mt-3 text-xs'>
+        <button>
+            <span className = 'opacity-50'>Followed by </span>
+            <span>{followedByMiddle}</span>
+            <span className = 'opacity-50'>{followedByEnd}</span>
+        </button>
+        
+    </div>
+  )
+}
+
+export default Mutuals
