7bb698a1b3d2edec500ae1de1e10dc1e46bac119
Thomas Booth <thomasbooth26@gmail.com>
2023-03-10 01:09:29 +0000

    Personal account taken into account

--- insta-remake/src/Components/Profile/Name.jsx
+++ insta-remake/src/Components/Profile/Name.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 function Name({personalAcc, handle, data, followedUser}) {
     
-  return (
-    <div>
-        <div className = 'flex items-center'>
-            
-            <span className = 'text-xl tracking-wide font-light'>{handle}</span>
-            {!data.followed 
-            ? <button onClick = {() => {followedUser()}} className = 'bg-blue-500 hover:bg-blue-600 font-[450] text-[14.5px] text-white py-1.5 px-5 ml-10 rounded-lg items-center'>Follow</button>
-            : <button onClick = {() => {followedUser()}}className = 'bg-gray-200 hover:bg-gray-300 font-[450] text-[14.5px] text-black-100 py-1.5 px-5 ml-10 rounded-lg items-center'>Following <i className = 'fa fa-lg fa-angle-down'></i></button>}
+    let userButtons = 
+    <div className = 'flex items-center'>
+        <button className = 'bg-gray-200 hover:bg-gray-300 font-[450] text-[14.5px] text-black-100 py-1.5 px-5 ml-10 rounded-lg items-center'>Edit Profile</button>
+        <button><i className = 'fa text-[20px] fa-cog ml-3'></i></button>
+    </div>
+    
+    if(!personalAcc){
+        userButtons = 
+        <div className = 'flex items-center'>{!data.followed 
+        ? <button onClick = {() => {followedUser()}} className = 'bg-blue-500 hover:bg-blue-600 font-[450] text-[14.5px] text-white py-1.5 px-5 ml-10 rounded-lg items-center'>Follow</button>
+        : <button onClick = {() => {followedUser()}}className = 'bg-gray-200 hover:bg-gray-300 font-[450] text-[14.5px] text-black-100 py-1.5 px-5 ml-10 rounded-lg items-center'>Following <i className = 'fa fa-lg fa-angle-down'></i></button>}
             <button className = 'bg-gray-200 hover:bg-gray-300 font-[450] text-[14.5px] text-black-100 py-1.5 px-5 ml-2 rounded-lg items-center'>Message</button>
             <button className = 'bg-gray-200 hover:bg-gray-300 font-[450] text-[14.5px] text-black-100 py-1.5 px-1.5 ml-2 rounded-lg items-center'><i className = 'fa fa-user-o px-1'></i></button>
             <button className = 'ml-4 flex items-center'>
@@ -17,6 +20,14 @@ function Name({personalAcc, handle, data, followedUser}) {
                 <i className = 'fa text-[4px] fa-circle pl-1'></i>
                 <i className = 'fa text-[4px] fa-circle pl-1'></i>
             </button>
+        </div>
+    }
+  return (
+    <div>
+        <div className = 'flex items-center'>
+            <span className = 'text-xl tracking-wide font-light'>{handle}</span>
+            {userButtons}
+            
         </div>
         <div className='my-10'></div>
         <ul className = 'flex text-lg tracking-wide '>
--- insta-remake/src/Containers/About.jsx
+++ insta-remake/src/Containers/About.jsx
@@ -1,26 +1,23 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import Name from '../Components/Profile/Name'
 import React from 'react'
+import Image from '../Components/Profile/Image'
 useAuth0
 function About({handle, data, watchedStory, followedUser}) {
     const {isAuthenticated, user} = useAuth0()
     let personalAcc = false
     if (isAuthenticated && user.nickname === handle){personalAcc = !personalAcc}
-    let storyClass = ''
-    if (!data.story.watched){
-        storyClass = 'ring-offset-[5px] ring-rose-900 ring-[3.5px]'
-    } else {
-        storyClass = 'ring-offset-[5px] ring-grey-500 ring-[2px]'
-    }
 
   return (
     
     isAuthenticated && <div>
         <div className = 'flex mt-8 items-center'>
-            {data.story.exists ? 
-            <button onClick = {() => {watchedStory()}} className = 'mr-20 ml-10'>
-                <img src = 'images/cat.jpg' className = {'w-[175px] rounded-full border border-gray-500 ' + storyClass}></img>
-            </button> : <div className = 'mr-20 ml-10'><img src = 'images/cat.jpg' className = 'w-[175px] rounded-full border border-gray-500'></img></div>}
+            {!personalAcc 
+                ? <Image data = {data} user = {user} watchedStory = {watchedStory}/>
+                : <button className = 'mr-20 ml-10'>
+                    <img src = {user.picture} className = {'w-[175px] rounded-full border border-gray-500'}></img>
+                </button>
+            }
             <div>
                 <Name personalAcc = {personalAcc} handle = {handle} data = {data} followedUser = {followedUser}/>
                 <div>
