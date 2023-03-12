3bedeac8bfc0bdceaa6f1feb10dbb5ac82ade949
Thomas Booth <thomasbooth26@gmail.com>
2023-03-12 18:04:59 +0000

    config change

--- insta-remake/vite.config.js
+++ insta-remake/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
-    port: 8080,
+    //host: '0.0.0.0',
+    //port: 8080,
     watch: {
         usePolling: true,
     }
