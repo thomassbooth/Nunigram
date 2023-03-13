d45757b03510525379010666a404f2ca41ff2fc1
Thomas Booth <thomasbooth26@gmail.com>
2023-03-13 23:30:35 +0000

    update opening page with auth0

--- insta-remake/src/Components/Auth0/Auth0login.jsx
+++ insta-remake/src/Components/Auth0/Auth0login.jsx
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   console.log("pls output")
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return loginWithRedirect();
 };
 
 export default LoginButton; 
