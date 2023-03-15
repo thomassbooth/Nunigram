4571a0d4f2dfd00b58a87433de0244de2a8e6413
Thomas Booth <thomasbooth26@gmail.com>
2023-03-15 22:36:52 +0000

    messages page

--- insta-remake/src/Pages/Messages.jsx
+++ insta-remake/src/Pages/Messages.jsx
@@ -5,10 +5,10 @@ function Messages() {
   return (
     <div className = 'w-screen h-screen bg-gray-50'>
       <div className = 'py-6 h-full w-full flex justify-center'>
-        <div className = 'flex h-full border border-gray-300 rounded w-[1000px] bg-white'>
+        <div className = 'flex h-full border border-gray-300 rounded-sm w-[1000px] bg-white'>
           <div className = 'w-[350px] border-r border-gray-300 h-full'>
             <div className = 'h-[60px] border-b border-gray-300'>
-
+              
             </div>
           </div>
           <div className = 'w-[650px] h-full'>
