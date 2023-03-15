2b540b5ba12444efb57bdf29f5fb7914ae316c93
Thomas Booth <thomasbooth26@gmail.com>
2023-03-15 20:56:16 +0000

    messages page updated

--- insta-remake/src/Pages/Messages.jsx
+++ insta-remake/src/Pages/Messages.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 function Messages() {
+  document.body.style.overflow = 'hidden' 
   return (
-    <div>Messages</div>
+    <div className = 'w-4/5 h-screen bg-gray-50'>
+      <div className = 'p-6 h-full w-full flex justify-center'>
+        <div className = 'h-full border border-gray-300 rounded w-[1000px] bg-white'>
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
