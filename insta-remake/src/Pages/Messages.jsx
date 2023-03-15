ae1f568bcfd3ee1850111d8b77b524c87f35f429
Thomas Booth <thomasbooth26@gmail.com>
2023-03-15 22:24:27 +0000

    column setup

--- insta-remake/src/Pages/Messages.jsx
+++ insta-remake/src/Pages/Messages.jsx
@@ -4,10 +4,17 @@ function Messages() {
   document.body.style.overflow = 'hidden' 
   return (
     <div className = 'w-screen h-screen bg-gray-50'>
-      <div className = 'p-6 h-full w-full flex justify-center'>
-        <div className = 'h-full border border-gray-300 rounded w-[1000px] bg-white'>
-          <div>
+      <div className = 'py-6 h-full w-full flex justify-center'>
+        <div className = 'flex h-full border border-gray-300 rounded w-[1000px] bg-white'>
+          <div className = 'w-[300px] border-r border-gray-300 h-full'>
+            <div className = 'h-[75px] border-b border-gray-300'>
 
+            </div>
+          </div>
+          <div className = 'w-[700px] h-full'>
+            <div className = 'h-[75px] border-b border-gray-300'>
+
+            </div>
           </div>
         </div>
       </div>
