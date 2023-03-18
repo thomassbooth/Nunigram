242507ec1b56db8175fedf0b3c4cdeed9df11f75
Thomas Booth <thomasbooth26@gmail.com>
2023-03-18 23:23:33 +0000

    more messages stuff

--- insta-remake/src/Components/Messages/Message.jsx
+++ insta-remake/src/Components/Messages/Message.jsx
@@ -5,12 +5,12 @@ function Message({messageData, openMessage, setOpenMessage}) {
     
     console.log(messageData)
     
-    if (openMessage === messageData.name){var backgroundStyle = 'bg-gray-100 py-3'}
+    if (openMessage.name === messageData.name){var backgroundStyle = 'bg-gray-100 py-3'}
 
   return (
-    <div onClick = {() => {setOpenMessage(messageData.name)}} className = {backgroundStyle ? backgroundStyle : 'hover:bg-gray-50 py-3'}>
+    <div onClick = {() => {setOpenMessage(messageData)}} className = {backgroundStyle ? backgroundStyle : 'hover:bg-gray-50 py-3'}>
         <div className = 'flex items-center'>
-            <img src = {messageData.image} className = ' h-14 rounded-full mx-3'></img>
+            <img src = {messageData.image} className = ' h-14 rounded-full ml-5 mr-3 border border-gray-400'></img>
             <div>
                 <span>{messageData.name}</span>
             </div>
--- insta-remake/src/Pages/Messages.jsx
+++ insta-remake/src/Pages/Messages.jsx
@@ -5,7 +5,7 @@ import Message from '../Components/Messages/Message'
 
 function Messages() {
   const {isAuthenticated, user} = useAuth0()
-  const [messages, setMessages] = useState([{name: 'domhedgetrim', image: 'images/cat.jpg' }, {name: 'poopni', image: 'images/cat.jpg' }, {name: 'thomassbooth', image: 'images/cat.jpg' }, {name: 'DaveyP', image: 'images/cat.jpg' }, {name: 'nunipooni', image: 'images/cat.jpg' }])
+  const [messages, setMessages] = useState([{nickname: 'domhedgetrim', name: 'Dom Hedges', image: 'images/cat.jpg' }, {nickname: 'poopni', name: 'poopni', image: 'images/storm.jpeg' }, {nickname: 'thomassbooth', name: 'Thomas Booth', image: 'images/cat.jpg' }, {nickname: 'DaveyP', name: 'Dave Payne', image: 'images/cat.jpg' }, {nickname: 'marioLinderrr', name: 'Marius Lindberg', image: 'images/cat.jpg' }])
   const [openMessage, setOpenMessage] = useState('')
 
   useEffect(() => {
@@ -61,9 +61,25 @@ function Messages() {
                     </button>
                   </div>
                 </div>
-              : <div className = 'h-[60px] border-b border-gray-300'>
-
-              </div>}
+              : <div className = 'h-full'>
+                  <div className = 'h-[60px] border-b border-gray-300 flex justify-between items-center'>
+                    <div className = 'flex items-center ml-9'>
+                      <img src = {openMessage.image} className = 'mr-3 rounded-full w-7 h-7'></img>
+                      <span className = 'font-medium'>{openMessage.name}</span>
+                    </div>
+                    <div className = 'flex space-x-4 mr-6'>
+                      <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                      </svg></button>
+                      <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                        <path strokeLinecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+                      </svg></button>
+                      <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                      </svg></button>
+                    </div>
+                  </div>
+                </div>}
           </div>
         </div>
       </div>
