15610939530dbba6a268d86a2008b4193cf686b6
Thomas Booth <thomasbooth26@gmail.com>
2023-03-21 00:41:30 +0000

    update button

--- insta-remake/src/Components/Messages/Message.jsx
+++ insta-remake/src/Components/Messages/Message.jsx
@@ -5,19 +5,18 @@ function Message({messageData, openMessage, setOpenMessage}) {
     
     console.log(messageData)
     
-    if (openMessage.name === messageData.name){var backgroundStyle = 'bg-gray-100 py-3'}
+    if (openMessage.name === messageData.name){var backgroundStyle = 'bg-gray-100 py-3 w-full'}
 
   return (
-    <div onClick = {() => {setOpenMessage(messageData)}} className = {backgroundStyle ? backgroundStyle : 'hover:bg-gray-50 py-3'}>
-        <div className = 'flex items-center'>
+    <button onClick = {() => {setOpenMessage(messageData)}} className = {backgroundStyle ? backgroundStyle : 'hover:bg-gray-50 py-3 w-full'}>
+        <div className = 'flex items-center float-left'>
             <img src = {messageData.image} className = ' h-14 rounded-full ml-5 mr-3 border border-gray-400'></img>
-            <div>
+            <div className = 'flex flex-col items-start'>
                 <p className = 'font-normal text-sm'>{messageData.name}</p>
                 <p className = 'font-normal text-sm opacity-40'>Active {messageData.active}</p>
             </div>
         </div>
-        
-    </div>
+    </button>
   )
 }
 
