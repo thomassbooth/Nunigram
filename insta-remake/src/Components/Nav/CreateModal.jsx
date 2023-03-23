100459fff2658e4bffc3d9c511b68fa0b08b4ae3
Thomas Booth <thomasbooth26@gmail.com>
2023-03-23 01:02:40 +0000

    tag style updated

--- insta-remake/src/Components/Nav/CreateModal.jsx
+++ insta-remake/src/Components/Nav/CreateModal.jsx
@@ -90,7 +90,7 @@ function CreateModal() {
                 {tags.map((tag, index) => {
                     let coords = imgRef.current.getBoundingClientRect()
                     console.log(tag)
-                    return <TagIcon key = {index} id = {index} x = {tag.x + (coords.left)} y = {tag.y + (coords.bottom)} submit_tag = {submit_tag}/>
+                    return <TagIcon key = {index} id = {index} x = {tag.x + (coords.left)} y = {tag.y + (coords.bottom)} submitted = {tag.submitted} submit_tag = {submit_tag}/>
                 })}
                 <img ref = {imgRef} onClick = {add_tag} className = 'border-r-[1px] border-gray-300 h-[560px] w-[560px]'src = {selectedFile.imagePreviewUrl}></img>
                 <div className = 'h-[560px] overflow-y-auto w-[340px]'>
@@ -105,7 +105,7 @@ function CreateModal() {
                     </div>}
                     <textarea onChange = {caption_change} rows = {1} maxLength = '2200' placeholder = 'Write a caption...' className = 'h-[150px] mx-3 font-light outline-none resize-none'/>
                     <div className = 'flex items-center justify-between m-3 text-xs opacity-30'>
-                        <button>
+                        <button className = 'z-0'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
                             </svg>
--- insta-remake/src/Components/Nav/TagIcon.jsx
+++ insta-remake/src/Components/Nav/TagIcon.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 
-function TagIcon({id, x, y, submit_tag}) {
+function TagIcon({id, x, y, submitted, submit_tag}) {
   return (
-    <div className = 'h-[30px] w-[100px] bg-gray-700 rounded-lg absolute flex' style ={{left: x-50, top: y}}>
-        
+    <div>
+    {submitted 
+    ? <div className = 'h-[30px] w-[100px] bg-gray-700 rounded-lg absolute flex' style ={{left: x-50, top: y}}>
         <input placeholder = 'Tag a friend' className = 'w-full font-light bg-transparent text-white outline-none'/>
         <button onClick = {() => {submit_tag(id)}}><i className = 'fa fa-check'></i></button>
-        
+    </div>
+    : <div className = 'w-[250px] shadow-xl z-10 bg-white rounded-lg absolute flex border' style ={{left: x-29, top: y+8}}>
+        <div className = 'h-[40px] w-full border-b flex items-center'>
+          <div className = ' bg-white w-[15px] h-[11px] rotate-45 -translate-y-5 translate-x-5'>
+          </div>
+          <span className = 'ml-2 mr-2 font-medium'>Tag:</span>
+          <input placeHolder = 'Search' className = 'placeholder-gray-500 w-full placeholder-opacity-30 text-sm outline-none'></input>
+          <span className = 'mx-2 opacity-25'><i className = 'fa fa-times-circle'></i></span>
+        </div>
+        <div className = 'h-[200px]'>
+        </div>
+      </div>}
     </div>
     
   )
